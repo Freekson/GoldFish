c6110a5789bdbdf2034758ebd4de428fc2498907
Yehor Dreval <freeksons@gmail.com>
2023-12-01 19:15:33 +0100

    add config files

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest",
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
   },
